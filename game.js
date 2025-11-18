166ef05b837c313dd30129c7e32a13b993836d17
dinodigger33 <hudsonasay@gmail.com>
2025-11-18 11:51:56 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
+// Simple test script
+alert("Game.js is connected!");
+
+// Draw something on the page
+const p = document.createElement("p");
+p.textContent = "JavaScript is running correctly.";
+document.body.appendChild(p);
 
